86c912a17476d8a1739f9597572193fdca839214
salma rami <ramisalma185@gmail.com>
2025-11-24 10:45:56 +0100

    11/24/2025 (commit#)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+     protected $fillable = [
+        'name', 'description', 'price',
+        'brand_id', 'category_id', 'gender_id', 'product_type_id'
+    ];
+
+    public function brand()  { return $this->belongsTo(Brand::class); }
+    public function category() { return $this->belongsTo(Category::class); }
+    public function gender() { return $this->belongsTo(Gender::class); }
+    public function type() { return $this->belongsTo(ProductType::class, 'product_type_id'); }
+
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class, 'product_colors')
+                    ->withPivot('id')
+                    ->using(ProductColor::class);
+    }
+
+    public function sizes()
+    {
+        return $this->belongsToMany(Size::class, 'product_sizes');
+    }
 }
--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product_type extends Model
+class ProductType extends Model
 {
     protected $fillable = ['name' , 'slug'] ;
     public function products(){
